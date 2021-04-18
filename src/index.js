d0cccc39ba98b8126cbda343f90e28e0d2ee0b7e
Lilian Zhao <milanzq309@gmail.com>
2021-04-18 12:24:59 -0700

    get layers and saved to display

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import LoginPage from './LoginPage.js'
 import Layer from './Layer.js'
+import Saved from './Saved.js'
 
 class Main extends React.Component {
     constructor(props) {
@@ -182,103 +183,6 @@ function LayerList(props) {
             </div>
         )
     }
-}
-
-class Saved extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            
-        }
-        this.copyCoords = this.copyCoords.bind(this)
-        this.copyLength = this.copyLength.bind(this)
-        this.animate = this.animate.bind(this)
-        this.deanimate = this.deanimate.bind(this)
-    }
-
-    render() {
-        return (
-            <div id={"saved" + this.props.number} className="layer saveLayer">
-                <div className="btn-box">
-                    <h2 className="longer-h2">{this.props.name}</h2>
-                    <div className="btn-box2">
-                        <button className="show" id={"ani" + this.props.number} onClick={this.animate}>Show</button>
-                    <button className="genNew" id={"ani" + this.props.number} onClick={this.deanimate}>Hide</button>
-                    </div>
-                </div>
-    
-                <div className="length-container">
-                    <div className="length-heading">
-                        <h3>Stroke Length</h3>
-                        <button className="copyLength" onClick={this.copyLength} id={"clBtn" + this.props.number}><i className="far fa-copy"></i></button>
-                        <h3 className="h3-margin-left">SVG Coordinates</h3>
-                        <button className="copyCoords" onClick={this.copyCoords} id={"ccBtn" + this.props.number}><i className="fas fa-copy"></i></button>
-                    </div>
-                </div>
-    
-                <div className="coords-container">
-                    <div className="coords-heading">
-                        <input readOnly value={this.props.length} className="small-input" id={"CL" + this.props.number} />
-                        <input readOnly value={this.props.path} className="small-input" id={"CC" + this.props.number} />
-                    </div>
-                </div>
-
-                <svg className="canvas" id="canvas2" width="600" height="600">
-                    <path id={"p" + this.props.number} className="draw saved-path" stroke={this.props.color} strokeLinecap="round" strokeWidth={this.props.strokeWidth} fill="transparent" d={this.props.path} />
-                </svg>
-            </div>
-        )
-    }
-
-    animate() {
-        let path = document.querySelector('#p' + this.props.number)
-        path.style.opacity = '100%'
-        if (!this.props.isScribble) {
-            path.style.strokeDasharray = this.props.length
-            path.style.strokeDashoffset = this.props.length
-            path.style.transition = ''
-            setTimeout(() => {
-                path.style.transition = `stroke-dashoffset ${this.props.animTime}s linear`
-                path.style.strokeDashoffset = 0
-            }, 100);
-            setTimeout(() => {
-                path.style.transition = ''
-            }, this.props.animTime * 1000);
-        } else {
-            path.style.strokeDasharray = this.props.length / 13
-            path.style.strokeDashoffset = this.props.length
-            path.style.animation = `scribble ${this.props.animTime * 6}s linear infinite alternate`
-        }
-    }
-
-    deanimate() {
-        let path = document.querySelector('#p' + this.props.number)
-        if (!this.props.isScribble) {
-            path.style.transition = ''
-            setTimeout(() => {
-                path.style.transition = `stroke-dashoffset ${this.props.animTime}s linear`
-                path.style.strokeDashoffset = this.props.length
-            }, 100);
-            setTimeout(() => {
-                path.style.transition = ''
-            }, this.props.animTime * 1000);
-        } else {
-            path.style.opacity = '0%'
-        }
-    }
-
-    //copy path coords
-    copyCoords() {
-        document.querySelector("#CC" + this.props.number).select()
-        document.execCommand("copy")
-    }
-
-    //copy path length
-    copyLength() {
-        document.querySelector("#CL" + this.props.number).select()
-        document.execCommand("copy")
-    }
-
 }
 
 
